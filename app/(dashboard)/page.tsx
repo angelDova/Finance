7ee8b32379119123a59ad56ed0c04ee2c0cb8c47
angelDova <Angelfcb01@yahoo.com>
2024-05-15 12:01:39 -0700

    sheet

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">Dashboard Page</div>;
+  const { onOpen } = useNewAccount();
+
+  return (
+    <div className="">
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
